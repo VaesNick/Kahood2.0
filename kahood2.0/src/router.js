8349f430df14bbbc902a9f590471fa6583c7f44c
steffenlen <steffen.lenaerts@student.pxl.be>
2019-01-15 16:13:38 +0100

    added login view

--- kahood2.0/src/router.js
+++ kahood2.0/src/router.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import CoursePage from './components/CoursePage'
 import LandingsPage from './components/LandingsPage'
-
+import LoginPage from './components/LoginPage'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
+        {
+            path:"/login",
+            name:"Login",
+            component: LoginPage
+        },
         {
             path: '/course/:id*',
             name: 'Course',
